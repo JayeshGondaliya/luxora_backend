a8996d120f27f1769e8df1d09b33cd8055ac0ccd
Jayesh Gondaliya <jayeshgondaliya@gmail.com>
2025-08-05 19:45:27 +0530

    save

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 export const connect=async()=>{
     try {
-        await mongoose.connect("mongodb+srv://jayeshgondaliya9929:Jayesh7574@cluster0.g3w2u.mongodb.net/luxora?retryWrites=true&w=majority&appName=Cluster0");
-    console.log("mongodb connected");
+        await mongoose.connect(process.env.MONGO_URL) ;
+    console.log("mongodb connected from server");
         
     } catch (error) {
 console.log("coonection error:-",error);
