0370dfa35affb8952f88ff9cc7f7306ab0915f07
Jayesh Gondaliya <jayeshgondaliya@gmail.com>
2025-08-06 10:44:07 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export const connect=async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URL) ;
+        await mongoose.connect("mongodb+srv://jayeshgondaliya9929:Jayesh7574@cluster0.g3w2u.mongodb.net/luxora?retryWrites=true&w=majority&appName=Cluster0") ;
     console.log("mongodb connected from server");
         
     } catch (error) {
