33670f63913f86498fc35033ceea00c31dbefc4f
JayeshGondaliya <166483981+JayeshGondaliya@users.noreply.github.com>
2025-08-14 18:49:42 +0530

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { getAllOrders, getOrder, recentOrder, saveOrder } from '../controllers/order.js'
-import { AdminAuthentication } from '../middleware/auth.js'
+import { AdminAuthentication,userAuthentication } from '../middleware/auth.js'
 const orderRouter=express.Router()
 orderRouter.post("/saveorder",saveOrder)
 orderRouter.get("/getorder/:userId",userAuthentication,getOrder)
