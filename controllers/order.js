b7978488eae741ffd5ac68f498476c351ea41d32
Jayesh Gondaliya <166483981+JayeshGondaliya@users.noreply.github.com>
2025-11-06 19:26:39 +0530

    changes

--- controllers/order.js
+++ controllers/order.js
@@ -8,7 +8,7 @@ export const saveOrder = async (req, res) => {
     if (existing) {
         return res.status(200).json({
             success: false,
-            message: "Order Already saved for this session.",
+            message: "Order Already saved for this Session.",
         });
     }
   const orderData = {
