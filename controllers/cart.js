871687b63d0cf2fd672a363efe362dd7c5838e27
Jayesh Gondaliya <166483981+JayeshGondaliya@users.noreply.github.com>
2025-12-20 22:56:51 +0530

    changes

--- controllers/cart.js
+++ controllers/cart.js
@@ -8,7 +8,7 @@ export const addToCart=async(req,res)=>{
             return res.status(400).json({success:false,message:"Please Provide All Details"})
         }
 
-        let cart=await Cart.findOne({userId})
+        let cart=await Cart.findOne({userId}
         //if cart exists
         if(cart){
                
